b15c3d88f0ab5da0af0672128bb8d50aa40f7dc1
DucTV <128478337+tranduchello@users.noreply.github.com>
2023-11-24 20:26:24 +0700

    duc24-11

---
+++ Assets/Scenes/Scrips/GameCleint.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameClient
+{
+    public abstract void StartGame();
+    public abstract void EndGame();
+    public abstract void Arsenal();
+    public abstract void Profile();
+}
---
+++ Assets/Scenes/Scrips/gameStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameStart : GameClient
+{
+    public override void Arsenal()
+    {
+        // triển khai logic của arsenal
+    }
+
+    public override void EndGame()
+    {
+        // triển  khailogic kết  thúc game
+    }
+
+    public override void StartGame()
+    {
+        // Triển khai logic bắt đầu trò chơi
+        Debug.Log("startgame");
+    }
+    public override void Profile()
+    {
+        // triển khai logic về profile
+    }
+}
